daa3d0b34b2f8789e6bd39f5626b6207c4b00feb
Gregg Caines <gcaines@exacttarget.com>
2012-07-24 15:03:43 -0700

    added mongo support?

--- core.js
+++ core.js
@@ -1,19 +1,38 @@
-//require('kafka');
-require('mongodb');
-
-
 var kafka = require('kafka');
 var Consumer = kafka.Consumer;
 var Producer = kafka.Producer;
-var TOPIC = 'test';
-
-
+var TOPIC = 'messages';
+var MONGO_HOST = '127.0.0.1';
+var MONGO_PORT = 27017;
+var MONGO_DB = 'messages';
+var KAFKA_HOST = '127.0.0.1';
+var KAFKA_PORT = 9092;
+var Mongo = require('mongodb');
 var consumer = new Consumer();
+
+
+// MONGO ---------------------
+console.log("Connecting to mongo at ", MONGO_HOST, ":", MONOGO_PORT, '/', MONGO_DB);
+var mongoClient = new Mongo.Db(MONGO_DB, new Mongo.Server(MONGO_HOST, MONGO_PORT, {}));
+
+
+function mongoInsert(mongoClient, doc){
+  mongoClient.collection('test_insert', function(err, collection){
+      collection.insert(doc, function(err, docs) {
+        if (err){
+          console.log("mongo insert error: ", err);
+          return;
+        }
+        console.log("inserted...", docs);
+      });
+  });
+}
+
 consumer.on('message', function(message) {
+  var parsed = JSON.parse(message);
   console.log('got a message');
-  if (message != 'hey too!'){
-    console.log("parsed: ", JSON.parse(message));
-  }
+  console.log("parsed: ", parsed);
+  mongoInsert(mongoClient, parsed);
 });
 
 consumer.on('lastmessage', function() {
@@ -32,10 +51,17 @@ consumer.on('offset', function() {
   console.log("consumer: offset");
 });
 
-consumer.connect(function() {
-  consumer.subscribeTopic({name: TOPIC, partition: 0});
-  console.log("there are ", consumer.topics(), " topics.");
-  //consumer.close();
+
+
+
+mongoClient.open(function(err, p_client) {
+
+  consumer.connect(function() {
+    consumer.subscribeTopic({name: TOPIC, partition: 0});
+    console.log("there are ", consumer.topics(), " topics.");
+    //consumer.close();
+  });
+
 });
 
 
@@ -44,8 +70,8 @@ consumer.connect(function() {
 var producer = new Producer(
     {
         // these are also the default values
-        host:         'localhost',
-        port:         9092,
+        host:         KAFKA_HOST,
+        port:         KAFKA_PORT,
         topic:        TOPIC,
         partition:    0
     }
@@ -66,6 +92,8 @@ producer.connect().on('connect', function() {
   }, 4000);
   // producer.close();
 });
+
+
 
 
 
