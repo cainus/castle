2fef79b45b66c5d89bdb6665a9d2f16e7d4c92b2
cainus <gregg@caines.ca>
2013-04-23 12:49:30 -0700

    got basics working.

--- test.js
+++ test.js
@@ -3,10 +3,10 @@ var Castle = require('./index').Castle;
 var config = {
   'http_port' : 8080,
   'kafka_port' : 9092,
-  'kafka_host' : 'localhost'
+  'kafka_host' : 'cfkafka01.stg.cotweet.com'
 };
 var server = new Castle(config);
-server.addTopic('catfish_qa', 1);
+server.addTopic('suckerfishstaging01', 1);
 server.listen(function(err){
   console.log("server listening on port ", server.port);
 });
