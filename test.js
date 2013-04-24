1b5cc4452938fc3d4644dcfb3356787344813cd5
cainus <gregg@caines.ca>
2013-04-23 22:44:50 -0700

    added some write capability.  added paging capability.

--- test.js
+++ test.js
@@ -3,10 +3,11 @@ var Castle = require('./index').Castle;
 var config = {
   'http_port' : 8080,
   'kafka_port' : 9092,
-  'kafka_host' : 'cfkafka01.stg.cotweet.com'
+  'kafka_host' : 'localhost'
 };
 var server = new Castle(config);
 server.addTopic('suckerfishstaging01', 1);
+server.addTopic('newtopic', 1);
 server.listen(function(err){
   console.log("server listening on port ", server.port);
 });
